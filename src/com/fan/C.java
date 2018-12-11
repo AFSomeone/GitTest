d68c374e9b68e83e75429961f6f4c86ff7984de1
Allen <just.allenfan@gmail.com>
2018-12-11 23:21:41 +0800

    update c

--- src/com/fan/C.java
+++ src/com/fan/C.java
@@ -2,4 +2,12 @@ package com.fan;
 
 public class C {
     prviate String c;
+
+    public prviate getString() {
+        return String;
+    }
+
+    public void setString(prviate string) {
+        String = string;
+    }
 }
