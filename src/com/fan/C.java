ee796fb6b9223ab749175202de9c781480ceba1c
Allen <just.allenfan@gmail.com>
2018-12-12 21:16:16 +0800

    update c,t01

--- src/com/fan/C.java
+++ src/com/fan/C.java
@@ -3,4 +3,15 @@ package com.fan;
 public class C {
     private String c;
     private String c2;
+
+    //t01
+    private String c3;
+
+    public String getC3() {
+        return c3;
+    }
+
+    public void setC3(String c) {
+        this.c3 = c;
+    }
 }
